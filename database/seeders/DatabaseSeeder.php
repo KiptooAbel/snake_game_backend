0e786972b0cc4bf2fa0fd34ac4d1b200aedbc8d7
KiptooAbel <4790abz@gmail.com>
2025-12-21 12:34:13 +0300

    seeder edit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
+            'username' => 'testuser',
             'email' => 'test@example.com',
+            'first_name' => 'Test',
+            'last_name' => 'User',
         ]);
     }
 }
